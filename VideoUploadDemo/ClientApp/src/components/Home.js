e0629712141a339307d4c3fb1adfae837c0f1747
Kyle <Kpaisley73@gmail.com>
2023-06-11 11:31:38 -0700

    Added VideoHelper.js to return a user's videos based on their userID.

--- VideoUploadDemo/ClientApp/src/components/Home.js
+++ VideoUploadDemo/ClientApp/src/components/Home.js
@@ -1,41 +1,50 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { getVideos } from '../helper/VideoHelper';
 
 
 export const Home = (props) => {
     const [videos, setVideos] = useState([]);
-
-    
+    const [videosLoading, setVideosLoading] = useState(true);
 
     function uploadVideo(e) {
         e.preventDefault();
         let files = e.target[0].files;
-
+        console.log(e.target[0].files);
         let reader = new FileReader();
         reader.readAsDataURL(files[0]);
 
         reader.onload = async (e) => {
-           
-            let videoToUpload = {
-                userID: 'ABC123',
-                videoTitle: 'TEST VIDEO 2',
-                videoStream: e.target.result
-            }
+            
+            /*NEED FIX THIS SO THAT NEW VIDEOS ARE ADDED TO videos.json FILE*/
+
+            //let videoToUpload = {
+            //    userID: 'ABC123',
+            //    videoTitle: 'TEST VIDEO 2',
+            //    videoStream: e.target.result
+            //}
 
 
-            const requestOptions = {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(videoToUpload)
-            }
+            //const requestOptions = {
+            //    method: "POST",
+            //    headers: { "Content-Type": "application/json" },
+            //    body: JSON.stringify(videoToUpload)
+            //}
 
-            await fetch('videos', requestOptions);
+            //await fetch('videos', requestOptions);
 
         }
     }
 
+    //Populate a user's videos
+    useEffect(() => {
+        const videos = getVideos('ABC123');
+        setVideos(videos);
+        setVideosLoading(false);
+    }, [])
+
   
     return (
-      <div>
+        <div>
         <h1>Hello, world!</h1>
             <form onSubmit={(e) => uploadVideo(e) }>
                 <input type="file" accept=".mp4" name="file"></input>
@@ -45,6 +54,17 @@ export const Home = (props) => {
                
                 
             </form>
+            {videos.map((vid) => (
+                <div key={vid.videoTitle}>
+                    <video controls width="500px">
+                        <source autoPlay type="video/mp4" src={vid.videoStream}></source>
+                    </video>
+                </div>
+                
+
+
+                
+            ))}
       </div>
     );
   
---
+++ VideoUploadDemo/ClientApp/src/helper/VideoHelper.js
@@ -0,0 +1,14 @@
+﻿
+
+export const getVideos = (userID) => {
+    const allVideos = require("../uploads/videos.json")
+    const userVideos = [];
+
+    for (let i = 0; i < allVideos.data.length; i++) {
+
+        if (allVideos.data[i].userID === userID) {
+            userVideos.push(allVideos.data[i])
+        }
+    }
+    return userVideos;
+}
